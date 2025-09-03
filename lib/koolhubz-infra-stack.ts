5f7356319cb6bc43238579775488ed2ab72b7972
Anthony Smith <antsmithdev@gmail.com>
2025-09-03 12:38:21 -0700

    adds cognito construct to stack

--- lib/koolhubz-infra-stack.ts
+++ lib/koolhubz-infra-stack.ts
@@ -1,13 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { CognitoConstruct } from './constructs/cognito-construct';
 
 interface KoolHubzStackProps extends cdk.StackProps {
   stage: string;
 }
 
 export class KoolHubzStack extends cdk.Stack {
+  public readonly cognito: CognitoConstruct
+
   constructor(scope: Construct, id: string, props: KoolHubzStackProps) {
     super(scope, id, props);
+
+    this.cognito = new CognitoConstruct(this, "Cognito", {
+      stage: props.stage
+    })
 
     // Tag all resources
     cdk.Tags.of(this).add('Project', 'KoolHubz');
